eda80d7cf5fdbd362c18f028cb7fd8af7b567de7
serede254 <162103298+serede254@users.noreply.github.com>
2024-03-19 13:55:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function distanceFromHqInBlocks(someValue){
 function distanceFromHqInFeet (someValue) {
     return distanceFromHqInBlocks(someValue)*264;
 }
-function distanceTravelledInFeet(a,b){
-    if(b>a){
-        return((b-a)*264);
+function distanceTravelledInFeet(c,d){
+    if(d>c){
+        return((d-c)*264);
     }
     else
-    return((a-b)*264);
+    return((c-d)*264);
 }
 function calculatesFarePrice(start,destination){
     let distance =distanceTravelledInFeet(start,destination);
